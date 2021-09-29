680d9737a1b25253889266fefc005fd9faf15284
Viana Lin <vianalin@pop-os.localdomain>
2021-09-28 21:44:49 -0400

    main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "euler.h"
+
+int main() {
+	printf("the answer for problem 1 is %d\n", problemOne());
+	printf("the answer for problem 6 is %d\n", problemSix());
+	return 0;
+}
